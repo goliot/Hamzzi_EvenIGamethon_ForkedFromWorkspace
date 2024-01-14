5d67e0582ff1d49266a62345f9df88b50a61f9b7
Jinlee0206 <ljs4784@gmail.com>
2024-01-15 08:09:19 +0900

    20240115_ScriptableObject_Card, UI_Font

--- Test Project/Assets/02.Scripts/CardData.cs
+++ Test Project/Assets/02.Scripts/CardData.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Object/ItemData")]
 public class CardData : ScriptableObject
 {
+    public enum CardType { MagicBolt, Boom, Aqua, Lumos, Exo, Momen, Fines}
+
     [Header("# Main Info")] // 
+    public CardType cardType;
     public int cardId; // 카드의 ID
-    public string cardName;
+    public string cardName; // 카드 이름
     public string cardDesc; // 아이템 설명
     public Sprite cardIcon; // 아이템의 UI를 담기위한 아이콘
-    
-    //[Header("# Level Data")]
 
-    //[Header("# Weapon?")] // 명칭 변경 추후 필요
+    [Header("# Level Data")]
+    public float baseDamage;  // 0레벨 기준 기본 공격력
+    public int baseCount;     // 0레벨 기준 개수
+    public float[] damages;
+    public int[] counts;
+
+    [Header("# Weapon?")] // 명칭 변경 추후 필요
+    public GameObject projectiles; // 투사체
+
 }
--- Test Project/Assets/02.Scripts/UI/Card.cs
+++ Test Project/Assets/02.Scripts/UI/Card.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
     [SerializeField] GameObject cardMenu;
+
+    public CardData cardData;
+    public int level;
+    public Player player;
+
+    Image icon;
+    Text textLevel;
+
+    private void Awake()
+    {
+        icon = GetComponentsInChildren<Image>()[1];
+        icon.sprite = cardData.cardIcon;
+
+        Text[] texts = GetComponentsInChildren<Text>();
+        textLevel = texts[0];
+    }
 
     private void Update()
     {
@@ -12,6 +29,45 @@ public class Card : MonoBehaviour
         {
             cardMenu.SetActive(true);
             Time.timeScale = 0;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        textLevel.text = "Lv." + (level + 1);
+    }
+
+    public void OnClick()
+    {
+        switch (cardData.cardType)
+        {
+            case CardData.CardType.MagicBolt:
+                // 마력구 스킬 1번 카드 증폭(피해)  : 피해량 +60%
+                // 마력구 스킬 2번 카드 증폭(폭발)  : 폭발 피해 +30%
+                // 마력구 스킬 3번 카드 관통        : 관통+2 / 피해 +10% 
+                // 마력구 스킬 4번 카드 쿨타임 감소 : -20%
+                // 마력구 스킬 5번 카드 더블 증폭   : 액서니아와 피해량 +40%
+                // 마력구 스킬 6번 카드 추가        : 폭발 / 관통 (이 선택지가 해금 되어야지 2번과 3번 선택지가 나온다
+                break;
+            case CardData.CardType.Boom:
+                break;
+            case CardData.CardType.Aqua:
+                break;
+            case CardData.CardType.Lumos:
+                break;
+            case CardData.CardType.Exo:
+                break;
+            case CardData.CardType.Momen:
+                break;
+            case CardData.CardType.Fines:
+                break;
+        }
+
+        level++;
+
+        if(level == cardData.damages.Length)
+        {
+            GetComponent<Button>().interactable = false;
         }
     }
 

6500cca280a7173a9edf38c05b829a7c9f126a86
Sumin Cha <109404269+goliot@users.noreply.github.com>
2024-01-21 22:20:38 +0900

    1차빌드 오류수정

--- Test Project/Assets/02.Scripts/Enemy.cs
+++ Test Project/Assets/02.Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
--- Test Project/Assets/02.Scripts/GameManager.cs
+++ Test Project/Assets/02.Scripts/GameManager.cs
@@ -51,6 +51,7 @@ public class GameManager : Singleton<GameManager>
         exp = 0;
         level = 0;
         nextExp = new int[]{ 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460, 480, 500 };
+        Application.targetFrameRate = 60;
     }
 
     private void Update()

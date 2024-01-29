f975050d91169d5ecc777cb021b7f5124a4de8a1
Sumin Cha <109404269+goliot@users.noreply.github.com>
2024-01-30 04:39:40 +0900

    V.2.1.2 - 진짜최종최종

--- Test Project/Assets/02.Scripts/GameManager.cs
+++ Test Project/Assets/02.Scripts/GameManager.cs
@@ -56,7 +56,7 @@ public class GameManager : Singleton<GameManager>
         nextExp = new int[]{ 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460, 480, 500 };
         Application.targetFrameRate = 60;
         isSelectingCard = false;
-    }
+    } //
 
     private void Update()
     {
